15c59d9472b872c72e1b143941bb01a346b65ebc
Adam Gardner <adam.gardner@dynatrace.com>
2025-08-25 12:16:52 +1000

    install kubectl

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -7,6 +7,7 @@ apt install -y python3-pip wget sudo gh
 
 # Install kubectl
 curl -LO "https://dl.k8s.io/release/v1.30.0/bin/linux/amd64/kubectl"
+sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
 
 # Install Helm
 curl -s https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3 | bash -
